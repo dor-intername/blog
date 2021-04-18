8beb70696adb6c6d91191bb173471426c8f8ca83
dor levy <ddl105095@gmail.com>
2021-04-18 17:35:32 +0300

    seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Post::factory(3)->create(['user_id'=>1]);
+        $user = User::factory(5)->create();
+        Post::factory(2)->create(['user_id'=>$user->id]);
         // \App\Models\User::factory(10)->create();
     }
 }
