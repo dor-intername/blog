1932772cb624bdf54066a1dd324fd3ac591156fb
dor levy <ddl105095@gmail.com>
2021-04-18 16:47:26 +0300

    add migrations

---
+++ database/migrations/2021_04_18_133851_create_comment_post_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentPostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comment_post', function (Blueprint $table) {
+            $table->primary(['comment_id','post_id']);
+            $table->unsignedBigInteger('comment_id');
+            $table->unsignedBigInteger('post_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comment_post');
+    }
+}
